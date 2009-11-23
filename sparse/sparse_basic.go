737be683d093c245e5c2b95c2d62707ee438090f
John Asmuth <jasmuth@gmail.com>
2009-11-23 17:29:19 -0500

    added more sparse operations, forgot to add a sparse file

---
+++ sparse/sparse_basic.go
@@ -0,0 +1,67 @@
+package matrix
+
+func (A *SparseMatrix) SwapRows(r1 int, r2 int) {
+	js := map[int]bool {};
+	for index := range A.elements {
+		i, j := A.getRowColIndex(index);
+		if i == r1 || i == r2 {
+			js[j] = true;
+		}
+	}
+	for j := range js {
+		tmp := A.Get(r1, j);
+		A.Set(r1, j, A.Get(r2, j));
+		A.Set(r2, j, tmp);
+	}
+}
+
+func (A *SparseMatrix) ScaleRow(r int, f float64) {
+	for index, value := range A.elements {
+		i, j := A.getRowColIndex(index);
+		if i == r {
+			A.Set(i, j, value*f);
+		}
+	}
+}
+
+func (A *SparseMatrix) ScaleAddRow(rd int, rs int, f float64) {
+	for index, value := range A.elements {
+		i, j := A.getRowColIndex(index);
+		if i == rs {
+			A.Set(rd, j, A.Get(rd, j)+value*f);
+		}
+	}
+}
+
+func (A *SparseMatrix) Symmetric() bool {
+	for index, value := range A.elements {
+		i, j := A.getRowColIndex(index);
+		if i != j && value != A.Get(j, i) {
+			return false;
+		}
+	}
+	return true;
+}
+
+func (A *SparseMatrix) Transpose() *SparseMatrix {
+	B := ZerosSparse(A.cols, A.rows);
+	for index, value := range A.elements {
+		i, j := A.getRowColIndex(index);
+		B.Set(j, i, value);
+	}
+	return B;
+}
+
+func (A *SparseMatrix) Det() float64 {
+	return 0;
+}
+
+func (A *SparseMatrix) Trace() (res float64) {
+	for index, value := range A.elements {
+		i, j := A.getRowColIndex(index);
+		if i == j {
+			res += value;
+		}
+	}
+	return;
+}
