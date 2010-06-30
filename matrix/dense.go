f5c74bd7e0240270a7de0db4ef395a65a937173b
John Asmuth <jasmuth@gmail.com>
2010-06-30 13:39:24 -0400

    if a matrix is not a submatrix, calling Arrays() will just slice the elements instead of copying it

--- matrix/dense.go
+++ matrix/dense.go
@@ -35,6 +35,9 @@ func (A *DenseMatrix) Arrays() [][]float64 {
 Returns the contents of this matrix stored into a flat array (row-major).
 */
 func (A *DenseMatrix) Array() []float64 {
+	if A.step == A.rows {
+		return A.elements[0:A.rows*A.cols]
+	}
 	a := make([]float64, A.rows*A.cols)
 	for i := 0; i < A.rows; i++ {
 		for j := 0; j < A.cols; j++ {
