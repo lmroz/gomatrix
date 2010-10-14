c1bcf68e043dbbff9a1be7f6a3ea5b99212773fd
John Asmuth <jasmuth@gmail.com>
2010-10-14 17:05:10 -0400

    saved v1.0

---
+++ 1.0/matrix/dense_arithmetic.go
@@ -0,0 +1,212 @@
+// Copyright 2009 The GoMatrix Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package matrix
+
+func (A *DenseMatrix) Plus(B MatrixRO) (Matrix, Error) {
+	C := A.Copy()
+	err := C.Add(B)
+	return C, err
+}
+func (A *DenseMatrix) PlusDense(B *DenseMatrix) (*DenseMatrix, Error) {
+	C := A.Copy()
+	err := C.AddDense(B)
+	return C, err
+}
+
+func (A *DenseMatrix) Minus(B MatrixRO) (Matrix, Error) {
+	C := A.Copy()
+	err := C.Subtract(B)
+	return C, err
+}
+
+func (A *DenseMatrix) MinusDense(B *DenseMatrix) (*DenseMatrix, Error) {
+	C := A.Copy()
+	err := C.SubtractDense(B)
+	return C, err
+}
+
+func (A *DenseMatrix) Add(B MatrixRO) Error {
+	if A.cols != B.Cols() || A.rows != B.Rows() {
+		return ErrorDimensionMismatch
+	}
+
+	for i := 0; i < A.rows; i++ {
+		index := i * A.step
+		for j := 0; j < A.cols; j++ {
+			A.elements[index] += B.Get(i, j)
+			index++
+		}
+	}
+
+	return NoError
+}
+
+func (A *DenseMatrix) AddDense(B *DenseMatrix) Error {
+	if A.cols != B.cols || A.rows != B.rows {
+		return ErrorDimensionMismatch
+	}
+
+	for i := 0; i < A.rows; i++ {
+		for j := 0; j < A.cols; j++ {
+			A.elements[i*A.step+j] += B.elements[i*B.step+j]
+		}
+	}
+
+	return NoError
+}
+
+func (A *DenseMatrix) Subtract(B MatrixRO) Error {
+	if Bd, ok := B.(*DenseMatrix); ok {
+		return A.SubtractDense(Bd)
+	}
+
+	if A.cols != B.Cols() || A.rows != B.Rows() {
+		return ErrorDimensionMismatch
+	}
+
+	for i := 0; i < A.rows; i++ {
+		index := i * A.step
+		for j := 0; j < A.cols; j++ {
+			A.elements[index] -= B.Get(i, j)
+			index++
+		}
+	}
+
+	return NoError
+}
+
+func (A *DenseMatrix) SubtractDense(B *DenseMatrix) Error {
+
+	if A.cols != B.cols || A.rows != B.rows {
+		return ErrorDimensionMismatch
+	}
+
+	for i := 0; i < A.rows; i++ {
+		indexA := i * A.step
+		indexB := i * B.step
+
+		for j := 0; j < A.cols; j++ {
+			A.elements[indexA] -= B.elements[indexB]
+			indexA++
+			indexB++
+		}
+	}
+
+	return NoError
+}
+
+func (A *DenseMatrix) Times(B MatrixRO) (Matrix, Error) {
+
+	if Bd, ok := B.(*DenseMatrix); ok {
+		return A.TimesDense(Bd)
+	}
+
+	if A.cols != B.Rows() {
+		return nil, ErrorDimensionMismatch
+	}
+	C := Zeros(A.rows, B.Cols())
+
+	for i := 0; i < A.rows; i++ {
+		for j := 0; j < B.Cols(); j++ {
+			sum := float64(0)
+			for k := 0; k < A.cols; k++ {
+				sum += A.elements[i*A.step+k] * B.Get(k, j)
+			}
+			C.elements[i*C.step+j] = sum
+		}
+	}
+
+	return C, NoError
+}
+
+func (A *DenseMatrix) TimesDense(B *DenseMatrix) (*DenseMatrix, Error) {
+	if A.cols != B.rows {
+		return nil, ErrorDimensionMismatch
+	}
+	C := Zeros(A.rows, B.cols)
+	///*
+	if MaxProcs > 1 {
+		wait := parFor(countBoxes(0, A.rows), func(iBox box) {
+			i := iBox.(int)
+			sums := C.elements[i*C.step : (i+1)*C.step]
+			for k := 0; k < A.Cols(); k++ {
+				for j := 0; j < B.Cols(); j++ {
+					sums[j] += A.elements[i*A.step+k] * B.elements[k*B.step+j]
+				}
+			}
+		})
+
+		wait()
+	} else {
+		for i := 0; i < A.rows; i++ {
+			for j := 0; j < B.cols; j++ {
+				sum := float64(0);
+				for k := 0; k < A.cols; k++ {
+					sum += A.elements[i*A.step+k] * B.elements[k*B.step+j]
+				}
+				C.elements[i*C.step+j] = sum;
+			}
+		}
+	}
+
+	return C, NoError
+}
+
+
+func (A *DenseMatrix) ElementMult(B MatrixRO) (Matrix, Error) {
+	C := A.Copy()
+	err := C.ScaleMatrix(B)
+	return C, err
+}
+
+func (A *DenseMatrix) ElementMultDense(B *DenseMatrix) (*DenseMatrix, Error) {
+	C := A.Copy()
+	err := C.ScaleMatrixDense(B)
+	return C, err
+}
+
+func (A *DenseMatrix) Scale(f float64) {
+	for i := 0; i < A.rows; i++ {
+		index := i * A.step
+		for j := 0; j < A.cols; j++ {
+			A.elements[index] *= f
+			index++
+		}
+	}
+}
+
+func (A *DenseMatrix) ScaleMatrix(B MatrixRO) Error {
+	if Bd, ok := B.(*DenseMatrix); ok {
+		return A.ScaleMatrixDense(Bd)
+	}
+
+	if A.rows != B.Rows() || A.cols != B.Cols() {
+		return ErrorDimensionMismatch
+	}
+	for i := 0; i < A.rows; i++ {
+		indexA := i * A.step
+		for j := 0; j < A.cols; j++ {
+			A.elements[indexA] *= B.Get(i, j)
+			indexA++
+		}
+	}
+	return NoError
+}
+
+func (A *DenseMatrix) ScaleMatrixDense(B *DenseMatrix) Error {
+	if A.rows != B.rows || A.cols != B.cols {
+		return ErrorDimensionMismatch
+	}
+	for i := 0; i < A.rows; i++ {
+		indexA := i * A.step
+		indexB := i * B.step
+		for j := 0; j < A.cols; j++ {
+			A.elements[indexA] *= B.elements[indexB]
+			indexA++
+			indexB++
+		}
+	}
+	return NoError
+}
---
+++ 1.0/matrix/matrix.go
@@ -0,0 +1,135 @@
+// Copyright 2009 The GoMatrix Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package matrix
+
+import (
+	"strings"
+	"fmt"
+)
+
+//The MatrixRO interface defines matrix operations that do not change the
+//underlying data, such as information requests or the creation of transforms
+/*
+Read-only matrix types (at the moment, PivotMatrix).
+*/
+type MatrixRO interface {
+	//Returns true if the underlying object is nil.
+	Nil() bool
+
+	//The number of rows in this matrix.
+	Rows() int
+	//The number of columns in this matrix.
+	Cols() int
+
+	//The number of elements in this matrix.
+	NumElements() int
+	//The size pair, (Rows(), Cols())
+	GetSize() (int, int)
+
+	//The element in the ith row and jth column.
+	Get(i, j int) float64
+
+	Plus(MatrixRO) (Matrix, Error)
+	Minus(MatrixRO) (Matrix, Error)
+	Times(MatrixRO) (Matrix, Error)
+
+	//The determinant of this matrix.
+	Det() float64
+	//The trace of this matrix.
+	Trace() float64
+
+	//A pretty-print string.
+	String() string
+
+	DenseMatrix() *DenseMatrix
+	SparseMatrix() *SparseMatrix
+}
+
+/*
+A mutable matrix.
+*/
+type Matrix interface {
+	MatrixRO
+
+	//Set the element at the ith row and jth column to v.
+	Set(i int, j int, v float64)
+
+	Add(MatrixRO) Error
+	Subtract(MatrixRO) Error
+	Scale(float64)
+}
+
+type matrix struct {
+	rows int
+	cols int
+}
+
+func (A *matrix) Nil() bool { return A == nil }
+
+func (A *matrix) Rows() int { return A.rows }
+
+func (A *matrix) Cols() int { return A.cols }
+
+func (A *matrix) NumElements() int { return A.rows * A.cols }
+
+func (A *matrix) GetSize() (rows, cols int) {
+	rows = A.rows
+	cols = A.cols
+	return
+}
+
+
+func String(A MatrixRO) string {
+	condense := func(vs string) string {
+		if strings.Index(vs, ".") != -1 {
+			for vs[len(vs)-1] == '0' {
+				vs = vs[0 : len(vs)-1]
+			}
+		}
+		if vs[len(vs)-1] == '.' {
+			vs = vs[0 : len(vs)-1]
+		}
+		return vs
+	}
+
+	if A == nil {
+		return "{nil}"
+	}
+	s := "{"
+
+	maxLen := 0
+	for i := 0; i < A.Rows(); i++ {
+		for j := 0; j < A.Cols(); j++ {
+			v := A.Get(i, j)
+			vs := condense(fmt.Sprintf("%f", v))
+
+			maxLen = maxInt(maxLen, len(vs))
+		}
+	}
+
+	for i := 0; i < A.Rows(); i++ {
+		for j := 0; j < A.Cols(); j++ {
+			v := A.Get(i, j)
+
+			vs := condense(fmt.Sprintf("%f", v))
+
+			for len(vs) < maxLen {
+				vs = " " + vs
+			}
+			s += vs
+			if i != A.Rows()-1 || j != A.Cols()-1 {
+				s += ","
+			}
+			if j != A.Cols()-1 {
+				s += " "
+			}
+		}
+		if i != A.Rows()-1 {
+			s += "\n "
+		}
+	}
+	s += "}"
+	return s
+}
