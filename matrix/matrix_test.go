3658516af2f134c37ff1395d38d9dbe47effc9a9
John Asmuth <jasmuth@gmail.com>
2011-10-28 23:09:13 -0400

    propogate misalign errors

--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -7,8 +7,9 @@ import (
 func TestParse(t *testing.T) {
 	s := `[1 2 3;4 5 6]`
 	A, err := ParseMatlab(s)
+
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	Ar := MakeDenseMatrix([]float64{1,2,3,4,5,6}, 2, 3)
 	if !Equals(A, Ar) {
